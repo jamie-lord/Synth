e2796e0fa0f5e2b410e5bd46cbe5284f3bfa7eb9
Jamie Lord <jamie@lord.technology>
2021-04-07 19:44:18 +0100

    Plays sine tone

---
+++ Synth/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace Synth
+{
+    public class Program
+    {
+        private const int SAMPLE_RATE = 44100;
+        private const short BITS_PER_SAMPLE = 16;
+
+        public static void Main(string[] args)
+        {
+            short[] wave = new short[SAMPLE_RATE];
+            float frequency = 440f;
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(Math.PI * 2 * frequency / SAMPLE_RATE * i));
+            }
+            byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)];
+            Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+            {
+                short blockAlign = BITS_PER_SAMPLE / 8;
+                int subChunkTwoSize = SAMPLE_RATE * blockAlign;
+                binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
+                binaryWriter.Write(36 + subChunkTwoSize);
+                binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
+                binaryWriter.Write(16);
+                binaryWriter.Write((short)1);
+                binaryWriter.Write((short)1);
+                binaryWriter.Write(SAMPLE_RATE);
+                binaryWriter.Write(SAMPLE_RATE * blockAlign);
+                binaryWriter.Write(blockAlign);
+                binaryWriter.Write(BITS_PER_SAMPLE);
+                binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
+                binaryWriter.Write(subChunkTwoSize);
+                binaryWriter.Write(binaryWave);
+                memoryStream.Position = 0;
+                new SoundPlayer(memoryStream).Play();
+                Console.ReadLine();
+            }
+        }
+    }
+}
